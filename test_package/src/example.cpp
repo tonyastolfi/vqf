a395c1d6f6c115bbead9f0b6f6f8cb85182121bb
Anthony Astolfi <tastolfi@gmail.com>
2025-04-06 08:03:26 -0400

    Update example.cpp to include the public headers.

--- test_package/src/example.cpp
+++ test_package/src/example.cpp
@@ -1,11 +1,13 @@
 #include "vqf/vqf.hpp"
+#include "vqf/vqf_filter.h"
+#include "vqf/vqf_wrapper.h"
 
 #include <iostream>
 
 int main()
 {
-    if (vqf::entry_point()) {
-        std::cout << "Test package is working!" << std::endl;
-    }
-    return 0;
+   if (vqf::entry_point()) {
+      std::cout << "Test package is working!" << std::endl;
+   }
+   return 0;
 }
